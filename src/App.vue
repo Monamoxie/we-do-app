ae820aa2e4561ef13a35d093c7d86bc843232382
Mona Moxie <moxie4lyf@gmail.com>
2020-02-27 00:26:53 +0100

    Updated Package.json

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,35 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <img alt="Vue logo" src="./assets/logo.png" class="logo">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <TodoList></TodoList>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// Define components here
+// import HelloWorld from './components/HelloWorld.vue'
+import TodoList from './components/TodoList.vue'
 
 export default {
   name: 'App',
+  //Register components here
   components: {
-    HelloWorld
+    // HelloWorld,
+    TodoList
   }
 }
 </script>
 
 <style>
+
+* {
+  box-sizing: border-box;
+}
+.container {
+  max-width: 600px;
+  margin: 0px auto;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +37,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.logo {
+  height: 75px;
+  display: block;
+  margin: 20px auto;
 }
 </style>
