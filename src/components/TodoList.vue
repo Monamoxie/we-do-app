7729f1f316c46c92b7e01b8072b723deef079172
Mona Moxie <moxie4lyf@gmail.com>
2020-02-27 13:57:41 +0100

    Todos counter feature

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,19 +1,28 @@
 <template>
   <div class="hello">
-    <input class="form-control todo-input" type="text" placeholder="What do you want to do?" 
-        v-model="newTodo" @keyup.enter="addTodo"/>
+    <input class="form-control todo-input" type="text" placeholder="What do you want to do?" v-model="newTodo" @keyup.enter="addTodo"/>
     <div class="row mt-3 todos-wrapper">
         <div v-for="(todo, index) in todos" :key="todo.id" class="todo-item">
 
             <div class="todo-item-left">
                 <input type="checkbox" v-model="todo.completed">
-                <div class="todo-item-label" v-if="!todo.editing" @dblclick="editTodo(todo)">{{ todo.title }}</div>
+                <div class="todo-item-label" v-if="!todo.editing" @dblclick="editTodo(todo)" 
+                    :class="{ completed: todo.completed }">{{ todo.title }}</div>
                 <input v-else class="form-control todo-item-edit" ref="edit" type="text" v-model="todo.title" 
                     @blur="submitEdit(todo)" @keyup.enter="submitEdit(todo)" @keyup.esc="cancelEdit(todo)" v-focus/>
             </div>
             
             <div class="remove-item" @click="removeTodo(index)">&times;</div>
         </div>
+    </div>
+
+    <div class="extra-container">
+        <div>
+            <label>
+                <input type="checkbox"> Check All
+            </label>
+        </div>
+        <div> {{ remaining }} items left</div>
     </div>
   </div>
 </template>
@@ -51,6 +60,12 @@ export default {
             },
         ]
     }
+  },
+
+  computed: {
+      remaining() {
+          return this.todos.filter(todo => !todo.completed).length
+      }
   },
 
   directives: {
@@ -102,63 +117,99 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss">
- .todo-input {
-    width: 100%;
-    padding: 10px 18px;
-    font-size: 18px;
-    margin-bottom: 16px;
+    .todo-input {
+        width: 100%;
+        padding: 10px 18px;
+        font-size: 18px;
+        margin-bottom: 16px;
 
-    &:focus {
-        outline: 0;
+        &:focus {
+            outline: 0;
+        }
     }
- }
 
- .todos-wrapper {
-    padding: 20px 10px; 
-    box-shadow: 0 12px 15px 0 rgba(0, 0, 0, 0.24), 0 17px 50px 0 rgba(0, 0, 0, 0.10);
- }
-
- .todo-item {
-    text-align: left;
-    margin-bottom: 20px;
-    padding: 10px 12px;
-    border-bottom: 1px dotted #ccc;
-    display: flex;
-    justify-content: space-between;
- }
-
- .remove-item {
-    cursor: pointer;
-    margin-left: 14px;
-    &:hover {
-        color: red;
-        font-weight: bold;
+    .todos-wrapper {
+        padding: 20px 10px; 
+        box-shadow: 0 12px 15px 0 rgba(0, 0, 0, 0.24), 0 17px 50px 0 rgba(0, 0, 0, 0.10);
     }
- }
 
- .todo-item-left {
-    display: flex;
-    align-items: center;
- }
- .todo-item-label {
-    padding: 10px;
-    border: 1px solid white;
-    margin-left: 12px;
- }
-
- .todo-item-edit { 
-    color: #2c3e50;
-    margin-left: 12px;
-    font-size: 16px;
-    width: 100%;
-    padding: 10px 12px;
-    border: 1px solid #ccc; 
-
-    &:focus {
-        outline: none;
+    .todo-item {
+        text-align: left;
+        margin-bottom: 20px;
+        padding: 10px 12px;
+        border-bottom: 1px dotted #ccc;
+        display: flex;
+        justify-content: space-between;
     }
- }
 
+    .remove-item {
+        cursor: pointer;
+        margin-left: 14px;
+        &:hover {
+            color: red;
+            font-weight: bold;
+        }
+    }
 
+    .todo-item-left {
+        display: flex;
+        align-items: center;
+    }
+    .todo-item-label {
+        padding: 10px;
+        border: 1px solid white;
+        margin-left: 12px;
+    }
 
+    .todo-item-edit { 
+        color: #2c3e50;
+        margin-left: 12px;
+        font-size: 16px;
+        width: 100%;
+        padding: 10px 12px;
+        border: 1px solid #ccc; 
+
+        &:focus {
+            outline: none;
+        }
+    }
+
+    .completed {
+        text-decoration: line-through;
+        color: grey;
+        opacity: 0.5;
+    }
+
+    .extra-container {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        font-size: 16px;
+        border-top: 2px solid lightgrey;
+        padding-top: 14px;
+        margin: 14px auto;
+    }
+
+    button {
+        font-size: 14px;
+        background-color: white;
+        appearance: none;
+
+        &:hover {
+        background: lightgreen;
+        }
+
+        &:focus {
+            outline: none;
+        }
+
+    }
+
+    .active {
+        background: lightgreen;
+    }
+
+    
+
+ 
 </style>
