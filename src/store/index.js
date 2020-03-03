37013c0c695db8e9fb519b441de6bb891fd21aac
Mona Moxie <moxie4lyf@gmail.com>
2020-03-03 11:46:07 +0100

    Published CORS config

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://192.168.0.100:8080/api';
+axios.defaults.baseURL = 'https://192.168.0.100:8080/';
 
 Vue.use(Vuex)
 
