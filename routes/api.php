2c2d0e55406042930211c0a9af9021db17a7bf51
Mona Moxie <moxie4lyf@gmail.com>
2020-03-03 11:09:27 +0100

    Todo API routes

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/todos', 'TodosController@index');
+Route::post('/todos', 'TodosController@store');
+Route::patch('/todos/{todo}', 'TodosController@update');
+Route::delete('/todos/{todo}', 'TodosController@destroy');
+
+
