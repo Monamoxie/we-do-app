788b66a2002e690b4e723b0b49350d344358d260
Mona Moxie <moxie4lyf@gmail.com>
2020-04-11 16:38:13 +0100

    Updated webpack mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .styles([
+    .styles([ 
         'src/assets/css/style.css'
     ], 'public/css/app.css') 
